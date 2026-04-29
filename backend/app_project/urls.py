02873d83e4cd737ed147d0b3ac8395ee6406117b
pOsdas <ratmir1099@mail.ru>
2026-04-30 01:09:59 +0300

    hotfix: added staticfiles to debug run

--- backend/app_project/urls.py
+++ backend/app_project/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from strawberry.django.views import GraphQLView
 
@@ -9,3 +11,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("graphql/", GraphQLView.as_view(schema=schema)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
