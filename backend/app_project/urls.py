50caad1a02a23637af5998cf8eb90ff7e79d83a8
pOsdas <ratmir1099@mail.ru>
2026-05-02 02:13:51 +0300

    feat(payments): connected payments to urls.py + added API_PREFIX, API_V1_PREFIX to all urls

--- backend/app_project/urls.py
+++ backend/app_project/urls.py
@@ -1,15 +1,22 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
 from strawberry.django.views import GraphQLView
 
+from app.core.config import get_settings
 from app.api.v1.schema import schema
 
+core_settings = get_settings()
+
+API_PREFIX = core_settings.api.prefix
+API_V1_PREFIX = core_settings.api.v1.prefix
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("graphql/", GraphQLView.as_view(schema=schema)),
+    path(f"{API_PREFIX}{API_V1_PREFIX}/admin/", admin.site.urls),
+    path(f"{API_PREFIX}{API_V1_PREFIX}/graphql/", GraphQLView.as_view(schema=schema)),
+    path(f"{API_PREFIX}{API_V1_PREFIX}/payments/", include("app.api.v1.payments.urls")),
 ]
 
 if settings.DEBUG:
